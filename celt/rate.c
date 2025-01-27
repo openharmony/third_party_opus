xiph/opus

Adds cubic codebook
For both high-bands and theta=0,pi/2 splits

celt/rate.c
@@ -678,8 +678,10 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
    if (encode) {
       VARDECL(opus_val16, flatE);
       VARDECL(int, Ncoef);
+      VARDECL(opus_val16, cap);
 
       ALLOC(flatE, tot_bands, opus_val16);
+      ALLOC(cap, tot_bands, opus_val16);
       ALLOC(Ncoef, tot_bands, int);
       for (i=start;i<end;i++) {
          Ncoef[i] = (m->eBands[i+1]-m->eBands[i])*C<<LM;
@@ -687,15 +689,18 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
       /* Remove the effect of band width, eMeans and pre-emphasis to compute the real (flat) spectrum. */
       for (i=start;i<end;i++) {
          flatE[i] = PSHR32(bandLogE[i] - GCONST(0.0625f)*m->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(i+5)*(i+5), DB_SHIFT-10);
+         cap[i] = QCONST16(12.f, 10);
       }
       if (C==2) {
          for (i=start;i<end;i++) {
             flatE[i] = MAXG(flatE[i], PSHR32(bandLogE[m->nbEBands+i] - GCONST(0.0625f)*m->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(i+5)*(i+5), DB_SHIFT-10));
          }
       }
+      flatE[end-1] += QCONST16(2.f, 10);
       if (qext_mode != NULL) {
          for (i=0;i<NB_QEXT_BANDS;i++) {
             Ncoef[end+i] = (qext_mode->eBands[i+1]-qext_mode->eBands[i])*C<<LM;
+            cap[end+i] = QCONST16(14.f, 10);
          }
          for (i=0;i<NB_QEXT_BANDS;i++) {
             flatE[end+i] = PSHR32(qext_bandLogE[i] - GCONST(0.0625f)*qext_mode->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(end+i+5)*(end+i+5), DB_SHIFT-10);
@@ -705,6 +710,7 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
                flatE[end+i] = MAXG(flatE[end+i], PSHR32(qext_bandLogE[NB_QEXT_BANDS+i] - GCONST(0.0625f)*qext_mode->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(end+i+5)*(end+i+5), DB_SHIFT-10));
             }
          }
+         for (i=0;i<NB_QEXT_BANDS;i++) flatE[end+i] = flatE[end+i] + QCONST16(3.f, 10);
       }
       /* Approximate fill level assuming all bands contribute fully. */
       sum = 0;
@@ -717,24 +723,24 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
       for (iter=0;iter<10;iter++) {
          sum = 0;
          for (i=start;i<tot_bands;i++)
-            sum += Ncoef[i] * MIN32(QCONST16(12.f, 10), MAX32(0, flatE[i]-fill));
+            sum += Ncoef[i] * MIN32(cap[i], MAX32(0, flatE[i]-fill));
          fill -= (SHL32(total, 10) - sum)/tot_samples;
       }
       for (i=start;i<tot_bands;i++) {
 #ifdef FIXED_POINT
-         depth[i] = PSHR32(MIN32(QCONST16(12.f, 10), MAX32(0, flatE[i]-fill)), 10-2);
+         depth[i] = PSHR32(MIN32(cap[i], MAX32(0, flatE[i]-fill)), 10-2);
 #else
-         depth[i] = (int)floor(.5+4*MIN32(QCONST16(12.f, 10), MAX32(0, flatE[i]-fill)));
+         depth[i] = (int)floor(.5+4*MIN32(cap[i], MAX32(0, flatE[i]-fill)));
 #endif
-         if (ec_tell_frac(ec) + 45 < ec->storage*8<<BITRES)
-            ec_enc_uint(ec, depth[i], 49);
+         if (ec_tell_frac(ec) + 47 < ec->storage*8<<BITRES)
+            ec_enc_uint(ec, depth[i], 57);
          else
             depth[i] = 0;
       }
    } else {
       for (i=start;i<tot_bands;i++) {
-         if (ec_tell_frac(ec) + 45 < ec->storage*8<<BITRES)
-            depth[i] = ec_dec_uint(ec, 49);
+         if (ec_tell_frac(ec) + 47 < ec->storage*8<<BITRES)
+            depth[i] = ec_dec_uint(ec, 57);
          else
             depth[i] = 0;
       }

openharmony/third_party_opus

celt/rate.c
@@ -678,8 +678,10 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
    if (encode) {
       VARDECL(opus_val16, flatE);
       VARDECL(int, Ncoef);
+      VARDECL(opus_val16, cap);
 
       ALLOC(flatE, tot_bands, opus_val16);
+      ALLOC(cap, tot_bands, opus_val16);
       ALLOC(Ncoef, tot_bands, int);
       for (i=start;i<end;i++) {
          Ncoef[i] = (m->eBands[i+1]-m->eBands[i])*C<<LM;
@@ -687,15 +689,18 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
       /* Remove the effect of band width, eMeans and pre-emphasis to compute the real (flat) spectrum. */
       for (i=start;i<end;i++) {
          flatE[i] = PSHR32(bandLogE[i] - GCONST(0.0625f)*m->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(i+5)*(i+5), DB_SHIFT-10);
+         cap[i] = QCONST16(12.f, 10);
       }
       if (C==2) {
          for (i=start;i<end;i++) {
             flatE[i] = MAXG(flatE[i], PSHR32(bandLogE[m->nbEBands+i] - GCONST(0.0625f)*m->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(i+5)*(i+5), DB_SHIFT-10));
          }
       }
+      flatE[end-1] += QCONST16(2.f, 10);
       if (qext_mode != NULL) {
          for (i=0;i<qext_end;i++) {
             Ncoef[end+i] = (qext_mode->eBands[i+1]-qext_mode->eBands[i])*C<<LM;
+            cap[end+i] = QCONST16(14.f, 10);
          }
          for (i=0;i<qext_end;i++) {
             flatE[end+i] = PSHR32(qext_bandLogE[i] - GCONST(0.0625f)*qext_mode->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(end+i+5)*(end+i+5), DB_SHIFT-10);
@@ -705,6 +710,7 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
                flatE[end+i] = MAXG(flatE[end+i], PSHR32(qext_bandLogE[NB_QEXT_BANDS+i] - GCONST(0.0625f)*qext_mode->logN[i] + SHL32(eMeans[i],DB_SHIFT-4) - GCONST(.0062f)*(end+i+5)*(end+i+5), DB_SHIFT-10));
             }
          }
+         for (i=0;i<qext_end;i++) flatE[end+i] = flatE[end+i] + QCONST16(3.f, 10);
       }
       /* Approximate fill level assuming all bands contribute fully. */
       sum = 0;
@@ -717,24 +723,24 @@ void clt_compute_extra_allocation(const CELTMode *m, const CELTMode *qext_mode, 
       for (iter=0;iter<10;iter++) {
          sum = 0;
          for (i=start;i<tot_bands;i++)
-            sum += Ncoef[i] * MIN32(QCONST16(12.f, 10), MAX32(0, flatE[i]-fill));
+            sum += Ncoef[i] * MIN32(cap[i], MAX32(0, flatE[i]-fill));
          fill -= (SHL32(total, 10) - sum)/tot_samples;
       }
       for (i=start;i<tot_bands;i++) {
 #ifdef FIXED_POINT
-         depth[i] = PSHR32(MIN32(QCONST16(12.f, 10), MAX32(0, flatE[i]-fill)), 10-2);
+         depth[i] = PSHR32(MIN32(cap[i], MAX32(0, flatE[i]-fill)), 10-2);
 #else
-         depth[i] = (int)floor(.5+4*MIN32(QCONST16(12.f, 10), MAX32(0, flatE[i]-fill)));
+         depth[i] = (int)floor(.5+4*MIN32(cap[i], MAX32(0, flatE[i]-fill)));
 #endif
-         if (ec_tell_frac(ec) + 45 < ec->storage*8<<BITRES)
-            ec_enc_uint(ec, depth[i], 49);
+         if (ec_tell_frac(ec) + 47 < ec->storage*8<<BITRES)
+            ec_enc_uint(ec, depth[i], 57);
          else
             depth[i] = 0;
       }
    } else {
       for (i=start;i<tot_bands;i++) {
-         if (ec_tell_frac(ec) + 45 < ec->storage*8<<BITRES)
-            depth[i] = ec_dec_uint(ec, 49);
+         if (ec_tell_frac(ec) + 47 < ec->storage*8<<BITRES)
+            depth[i] = ec_dec_uint(ec, 57);
          else
             depth[i] = 0;
       }