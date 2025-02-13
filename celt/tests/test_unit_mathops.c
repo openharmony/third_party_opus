xiph/opus

Update the celt_exp2_db to match the new floating point celt_exp2.
Change-Id: Ib016fc8f0066980e3e5122b3f286680adc228ff9
Signed-off-by: Jean-Marc Valin <jeanmarcv@google.com>

celt/tests/test_unit_mathops.c
@@ -335,6 +335,42 @@ void testexp2(void)
    }
 }
 
+void testexp2_db(void)
+{
+#if defined(ENABLE_QEXT)
+   float absolute_error = -1;
+   float absolute_error_threshold = FIX_INT_TO_DOUBLE(2, 16);
+   float relative_error_threshold = -2;
+   float fx;
+   float quantized_fx;
+   float ground_truth;
+   opus_val32 x_32;
+
+   for (fx = -32.0; fx < 15.0; fx += 0.0007)
+   {
+      x_32 = DOUBLE_TO_FIX_INT(fx, DB_SHIFT);
+      quantized_fx = FIX_INT_TO_DOUBLE(x_32, DB_SHIFT);
+
+      double ground_truth = (exp(0.6931471805599453094 * quantized_fx));
+      absolute_error = fabs(ground_truth -
+                            FIX_INT_TO_DOUBLE(celt_exp2_db(x_32), 16));
+
+      relative_error_threshold = 1.24e-7 * ground_truth;
+      if (absolute_error > absolute_error_threshold &&
+          absolute_error > relative_error_threshold)
+      {
+         fprintf(stderr,
+                 "celt_exp2_db failed: "
+                 "absolute_error: [%.5e > %.5e] "
+                 "relative_error: [%.5e > %.5e] (x = %f)\n",
+                 absolute_error, absolute_error_threshold,
+                 absolute_error, relative_error_threshold, quantized_fx);
+         ret = 1;
+      }
+   }
+#endif  /* defined(ENABLE_QEXT) */
+}
+
 void testexp2log2(void)
 {
    opus_val32 x;
@@ -386,6 +422,7 @@ int main(void)
 #ifdef FIXED_POINT
    testilog2();
    testlog2_db();
+   testexp2_db();
 #else
    test_cos();
    test_atan2();

openharmony/third_party_opus

celt/tests/test_unit_mathops.c
@@ -276,6 +276,42 @@ void testexp2(void)
    }
 }
 
+void testexp2_db(void)
+{
+#if defined(ENABLE_QEXT)
+   float absolute_error = -1;
+   float absolute_error_threshold = FIX_INT_TO_DOUBLE(2, 16);
+   float relative_error_threshold = -2;
+   float fx;
+   float quantized_fx;
+   opus_val32 x_32;
+
+   for (fx = -32.0; fx < 15.0; fx += 0.0007)
+   {
+      double ground_truth;
+      x_32 = DOUBLE_TO_FIX_INT(fx, DB_SHIFT);
+      quantized_fx = FIX_INT_TO_DOUBLE(x_32, DB_SHIFT);
+
+      ground_truth = (exp(0.6931471805599453094 * quantized_fx));
+      absolute_error = fabs(ground_truth -
+                            FIX_INT_TO_DOUBLE(celt_exp2_db(x_32), 16));
+
+      relative_error_threshold = 1.24e-7 * ground_truth;
+      if (absolute_error > absolute_error_threshold &&
+          absolute_error > relative_error_threshold)
+      {
+         fprintf(stderr,
+                 "celt_exp2_db failed: "
+                 "absolute_error: [%.5e > %.5e] "
+                 "relative_error: [%.5e > %.5e] (x = %f)\n",
+                 absolute_error, absolute_error_threshold,
+                 absolute_error, relative_error_threshold, quantized_fx);
+         ret = 1;
+      }
+   }
+#endif  /* defined(ENABLE_QEXT) */
+}
+
 void testexp2log2(void)
 {
    opus_val32 x;
@@ -327,6 +363,7 @@ int main(void)
 #ifdef FIXED_POINT
    testilog2();
    testlog2_db();
+   testexp2_db();
 #endif
    return ret;
 }